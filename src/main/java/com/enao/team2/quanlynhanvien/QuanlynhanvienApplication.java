a9df06c884caed2f620993eb8070df5163c051a8
TuanHuyLe <lehuytuan20399@gmail.com>
2020-09-21 14:38:42 +0700

    first commit

---
+++ src/main/java/com/enao/team2/quanlynhanvien/QuanlynhanvienApplication.java
@@ -0,0 +1,13 @@
+package com.enao.team2.quanlynhanvien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuanlynhanvienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuanlynhanvienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enao/team2/quanlynhanvien/QuanlynhanvienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enao.team2.quanlynhanvien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlynhanvienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
