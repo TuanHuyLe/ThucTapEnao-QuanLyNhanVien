7bc02f4f980663afbdb348b5494070e55ad1dd10
Quốc Trường <22729763+luutruongquoc@users.noreply.github.com>
2020-10-02 14:00:40 +0700

    Update IRoleRepository.java

--- src/main/java/com/enao/team2/quanlynhanvien/repository/IRoleRepository.java
+++ src/main/java/com/enao/team2/quanlynhanvien/repository/IRoleRepository.java
@@ -4,11 +4,12 @@ import com.enao.team2.quanlynhanvien.constants.ERoles;
 import com.enao.team2.quanlynhanvien.model.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface IRoleRepository extends JpaRepository<RoleEntity, UUID> {
+public interface IRoleRepository extends JpaRepository<RoleEntity, UUID>, JpaSpecificationExecutor<RoleEntity> {
     Optional<RoleEntity> findByName(String name);
 }
